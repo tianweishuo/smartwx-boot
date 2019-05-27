3217b25bd255a6002a183d13a77087c7a4683eed
tianweishuo <756039422@qq.com>
2019-05-27 23:27:58 +0800

    小程序

--- userside/pages/basics/callcar/callcar.js
+++ userside/pages/basics/callcar/callcar.js
@@ -7,14 +7,13 @@ Page({
   data: {
     latitude: 0,
     longitude: 0,
-    openSetting:'',
+    openSetting: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -39,7 +38,7 @@ Page({
           longitude: res.longitude
         })
       },
-      fail:function(){
+      fail: function() {
         console.log("拒绝");
         that.setData({
           openSetting: "openSetting"
@@ -51,32 +50,27 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
-  },
+  onShareAppMessage: function() {},
   /**
    * 一键叫车
    */
@@ -92,12 +86,12 @@ Page({
             success: function(res) {
               console.log(JSON.stringify(res));
               wx.navigateTo({
-                url:"/pages/waitforcar/waitforcar"
+                url: "/pages/waitforcar/waitforcar"
               })
             }
           })
         }
       }
     })
-    }
+  }
 })
